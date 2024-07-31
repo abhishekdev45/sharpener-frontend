a4cd69d8873781b5d89456e882c80f4ebac96331
abhishekdev45 <156638090+AbhishekWebdesys@users.noreply.github.com>
2024-07-31 21:15:50 +0530

    Adding a Form

--- restaurant-website/src/components/Meals/MealItem/MealItem.js
+++ restaurant-website/src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,20 @@
-
+import { useContext } from "react";
+import CartContext from "../../../store/Cart-Context";
 import classes from "./MealItem.module.css";
-
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
-
+    const cartContext = useContext(CartContext);
     const price = `$${props.price.toFixed(2)}`;
 
+    const addToCartHandler = (amount) => {
+        cartContext.additem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price,
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -14,7 +23,7 @@ const MealItem = (props) => {
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
             </div>
-            
+            <MealItemForm onAddToCart={addToCartHandler} />
         </li>
     );
 };
