1b0652bcaa53a9b4899492996486efb41d44f355
abhishekdev45 <156638090+AbhishekWebdesys@users.noreply.github.com>
2024-08-03 10:38:13 +0530

    signup

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
