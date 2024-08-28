eb45497fd2573bd741c24d90171590d8dc8a6cdc
abhishekdev45 <156638090+AbhishekWebdesys@users.noreply.github.com>
2024-08-28 11:37:57 +0530

    Making the Details page work properly

--- nextjs/meetup/pages/[meetupId]/index.js
+++ nextjs/meetup/pages/[meetupId]/index.js
@@ -15,7 +15,7 @@ function MeetupDetails(props) {
 
 export async function getStaticPaths() {
   const client = await MongoClient.connect(
-    'mongodb+srv://maximilian:arlAapzPqFyo4xUk@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority'
+    process.env.NEXT_PUBLIC_MONGO_URL
   );
   const db = client.db();
 
@@ -39,7 +39,7 @@ export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
   const client = await MongoClient.connect(
-    'mongodb+srv://maximilian:arlAapzPqFyo4xUk@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority'
+    process.env.NEXT_PUBLIC_MONGO_URL
   );
   const db = client.db();
 
